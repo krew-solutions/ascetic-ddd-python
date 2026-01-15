82114e4109a9d6209c2447f94418a69ed09ff223
Ivan Zakrevsky <ivzak@yandex.ru>
2026-01-16 01:43:06 +0300

    Add InMemoryRepository

---
+++ ascetic_ddd/faker/infrastructure/repositories/in_memory_repository.py
@@ -0,0 +1,51 @@
+import typing
+
+from ascetic_ddd.seedwork.domain.identity.interfaces import IAccessible
+from ascetic_ddd.faker.domain.session.interfaces import ISession
+from ascetic_ddd.faker.domain.specification.interfaces import ISpecification
+
+__all__ = ('InMemoryRepository',)
+
+T = typing.TypeVar("T", covariant=True)
+
+
+class InMemoryRepository(typing.Generic[T]):
+    _id_attr: str = None
+    _aggregates: dict[T]
+    _agg_exporter: typing.Callable[[T], dict]
+
+    def __init__(self, agg_exporter: typing.Callable[[T], dict], id_attr: str = None):
+        self._agg_exporter = agg_exporter
+        self._id_attr = id_attr
+        self._aggregates = dict()
+
+    async def insert(self, session: ISession, agg: T):
+        state = self._agg_exporter(agg)
+        id_ = self._id(state)
+        self._aggregates[id_] = agg
+
+    async def get(self, session: ISession, id_: IAccessible[typing.Any]) -> T | None:
+        return self._aggregates.get(id_)
+
+    async def find(self, session: ISession, specification: ISpecification) -> typing.Iterable[T]:
+        """Add index support by extending the class"""
+        for id_, agg in self._aggregates.items():
+            if specification.is_satisfied_by(agg):
+                yield agg
+
+    async def setup(self, session: ISession):
+        pass
+
+    async def cleanup(self, session: ISession):
+        pass
+
+    def _id(self, state: dict) -> typing.Any:
+        if self._id_attr is not None:
+            return state.get(self._id_attr)
+        return next(iter(state.values()))
+
+    def __copy__(self):
+        return self
+
+    def __deepcopy__(self, memodict={}):
+        return self
